de34dbbb5b57c5f506bf38d50da929612aa65670
kavidu717 <dushmanthakavidu143@gmail.com>
2026-04-08 09:58:45 +0530

    set up the  first route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
-import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import MainLayout from './Layout/MainLayout'
+import Home from './Pages/Home'
 
 function App() {
- 
-
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
